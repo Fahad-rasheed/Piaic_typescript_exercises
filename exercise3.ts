e94380304ee3b573f6e9a812f246ae654c4d8c12
Fahad-rasheed <dentixtfahad@gmail.com>
2023-10-20 23:21:04 +0500

    update exercise3

---
+++ exercise3.ts
@@ -0,0 +1,22 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName : string = "FahAd RasHeed"
+
+let lowerCase :string=personName.toLowerCase();
+console.log(lowerCase)
+
+let upperCase :string = personName.toUpperCase();
+console.log(upperCase)
+
+
+
+let word :string []=personName.split(" ");
+let titlecase :string = ""
+for (let i = 0 ; i < word.length; i++){
+    titlecase +=word[i].charAt(0).toUpperCase()+word[i].slice(1).toLowerCase()+" "
+};
+console.log(titlecase)
+
+
+
+export {}
