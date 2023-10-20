e4818370135d010d079a6dcc59e8f20aad25c634
Fahad-rasheed <dentixtfahad@gmail.com>
2023-10-21 00:23:18 +0500

    update exercise5

---
+++ exercise5.ts
@@ -0,0 +1,12 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+//  Then compose your message and store it in a new variable called message.
+//  Print your message.
+
+
+
+// Quaid-e-Azam's said, Before you make a decision… “Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man.”
+
+let famous_person : string = "Quaid-e-Azam ";
+let message :string = famous_person + 'said, Before you make a decision… "Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man."'
+
+console.log(message)
