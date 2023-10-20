63c875f47789b81cafe58e8a3bcbe5294e7acc92
Fahad-rasheed <dentixtfahad@gmail.com>
2023-10-21 00:31:35 +0500

    update exercise7

---
+++ exercise7.ts
@@ -0,0 +1,22 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
+// Be sure to enclose your operations in print statements to see the results.
+
+// addition
+const additionResult = 3 + 5;
+
+console.log("Addition Result: "+ additionResult)
+
+// subtraction
+
+const subtrictionResult = 10 - 2;
+
+console.log("Subtraction Result: " +subtrictionResult);
+
+// multiplication
+const multiplicationResult= 2 * 4;
+console.log("Multiplication Result :"+multiplicationResult);
+
+//Division
+
+const divisionResult = 16 / 2;
+console.log("Division Result :" + divisionResult)
