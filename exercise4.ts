fbc90d6e684b7b4874b9d88f06cb2d588612b93c
Fahad-rasheed <dentixtfahad@gmail.com>
2023-10-21 00:19:12 +0500

    update Exercise4

---
+++ exercise4.ts
@@ -0,0 +1,9 @@
+// Famous Quote: Find a quote from a famous person you admire.
+//  Print the quote and the name of its author. 
+//  Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+
+// quaid-e-azam said, Before you make a decision… “Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man.”
+
+console.log('Quaid-e-Azam said, Before you make a decision… "Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man."')
