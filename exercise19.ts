2e7daa28c403e5421eaecd27b46d9ea24a5a7d83
Fahad-rasheed <dentixtfahad@gmail.com>
2023-10-21 01:57:08 +0500

    update exercise19

---
+++ exercise19.ts
@@ -0,0 +1,7 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+// Importing GuestList from exercise 15
+import { guestlist } from './exercise15'
+console.log(`\n Printing message indication number of guest`)
+
+console.log(` we are finally invited ${guestlist.length} guest in Exercise 15`)
